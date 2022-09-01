b27c124979ae911ce33dc463415c7891a3934b86
CamBrown101 <cameron-brown@live.com>
2022-09-01 14:36:33 -0700

    Fix codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
-require 'simplecov-lcov' 
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true 
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter SimpleCov.start
 SimpleCov.start
 
